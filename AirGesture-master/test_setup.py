9802da206e7807c3dc9168eff07e5da345211edd
Manh Cuong Pham <phammanhcuongada@gmail.com>
2026-04-25 04:51:29 +0700

    Clean commit: Only code and resources

---
+++ AirGesture-master/test_setup.py
@@ -0,0 +1,11 @@
+import cv2
+import mediapipe as mp
+import tensorflow as tf
+import numpy as np
+import cvzone
+
+print("--- Kiểm tra cài đặt ---")
+print(f"OpenCV version: {cv2.__version__}")
+print(f"TensorFlow version: {tf.__version__}")
+print(f"Numpy version: {np.__version__}")
+print("--- Tất cả đã sẵn sàng! ---")
